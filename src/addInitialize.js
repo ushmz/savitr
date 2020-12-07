6a76bea05131ba67127c82a036014de4f6608b7f
Yusuke Shimizu <shimizu@design.inf.shizuoka.ac.jp>
2020-12-08 00:57:51 +0900

    🗑 Remove unuse files

--- src/addInitialize.js
+++
@@ -1,9 +0,0 @@
-import {initializeTable, initializeHistory} from './IdxDB';
-
-document.getElementById('initBtn').addEventListener('click', async () => {
-  await initializeTable();  
-})
-
-document.getElementById('histBtn').addEventListener('click', async () => {
-  await initializeHistory();
-})
