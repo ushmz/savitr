c7e35f21a9eebb3551e42df2789edc550633ff93
Yusuke Shimizu <shimizu@design.inf.shizuoka.ac.jp>
2020-12-09 17:15:35 +0900

    :+1: Add header/footer

--- src/shared/Footer.tsx
+++ src/shared/Footer.tsx
@@ -1,5 +1,53 @@
 import React from 'react';
+import { MDBContainer, MDBFooter } from 'mdbreact';
 
-export const Footer: React.FC = () => {
-  return <></>;
+type Pages = 'Attention' | 'Introduntion' | 'PreTask' | 'Task' | 'PostTask';
+
+type Props = {
+  setPage: React.Dispatch<React.SetStateAction<Pages>>;
+};
+
+export const Footer: React.FC<Props> = ({ setPage }) => {
+  return (
+    <MDBFooter color="blue" className="font-small pt-4 mt-4">
+      <MDBContainer fluid className="text-center text-md-left">
+        <button
+          onClick={() => {
+            setPage('Attention');
+          }}
+        >
+          はじめに
+        </button>
+        <button
+          onClick={() => {
+            setPage('Introduntion');
+          }}
+        >
+          タスク説明
+        </button>
+        <button
+          onClick={() => {
+            setPage('PreTask');
+          }}
+        >
+          事前アンケート
+        </button>
+        <button
+          onClick={() => {
+            setPage('PostTask');
+          }}
+        >
+          事後アンケート
+        </button>
+        <button
+          onClick={() => {
+            setPage('Task');
+          }}
+        >
+          タスク開始
+        </button>
+      </MDBContainer>
+      <div className="footer-copyright text-center py-3"></div>
+    </MDBFooter>
+  );
 };
---
+++ src/shared/Header.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import { MDBNavbar, MDBNavbarBrand, MDBNavbarNav, MDBNavItem } from 'mdbreact';
+
+type Pages = 'Attention' | 'Introduntion' | 'PreTask' | 'Task' | 'PostTask';
+
+type Props = {
+  title: string;
+  setPage: React.Dispatch<React.SetStateAction<Pages>>;
+};
+
+export const Header: React.FC<Props> = ({ title, setPage }) => {
+  return (
+    <MDBNavbar color="blue" dark expand="md">
+      <MDBNavbarBrand>
+        <strong className="white-text">{title}</strong>
+      </MDBNavbarBrand>
+      <MDBNavbarNav left className={`${title.toLocaleLowerCase().match(/.*admin.*/) ? 'visible' : 'invisible'}`}>
+        <MDBNavItem active>
+          <div
+            className="white-text mx-2"
+            onClick={() => {
+              setPage('Attention');
+            }}
+          >
+            はじめに
+          </div>
+        </MDBNavItem>
+        <MDBNavItem active>
+          <div
+            className="white-text mx-2"
+            onClick={() => {
+              setPage('Introduntion');
+            }}
+          >
+            タスク説明
+          </div>
+        </MDBNavItem>
+        <MDBNavItem active>
+          <div
+            className="white-text mx-2"
+            onClick={() => {
+              setPage('PreTask');
+            }}
+          >
+            事前アンケート
+          </div>
+        </MDBNavItem>
+        <MDBNavItem active>
+          <div
+            className="white-text mx-2"
+            onClick={() => {
+              setPage('PostTask');
+            }}
+          >
+            事後アンケート
+          </div>
+        </MDBNavItem>
+        <MDBNavItem active>
+          <div
+            className="white-text mx-2"
+            onClick={() => {
+              setPage('Task');
+            }}
+          >
+            タスク開始
+          </div>
+        </MDBNavItem>
+      </MDBNavbarNav>
+      <div className="footer-copyright text-center py-3"></div>
+    </MDBNavbar>
+  );
+};
