42e77c3ac82930af0e835375ba4820e5737727ad
Yusuke Shimizu <shimizu@design.inf.shizuoka.ac.jp>
2020-12-21 19:30:08 +0900

    :+1: Complete 32cd05d

--- src/service/getAllHistory.ts
+++ src/service/getAllHistory.ts
@@ -1,54 +1,53 @@
-// let nextEndTimeToUse = 0;
-// let allItems = [];
-// let itemIdToIndex = {};
+import { HistoryAPIParams } from 'shared/types';
 
-// /**
-//  *
-//  * @param {callback} callback - Callback exetuted when once request finished
-//  */
-// function getMoreHistory(callback) {
+let nextEndTimeToUse: number | undefined = 0;
+const allItems: Array<chrome.history.HistoryItem> = [];
+const itemIdToIndex: { [key: string]: unknown } = {};
 
-//   const params = {text:"", maxResults:500};
-//   params.startTime = 0;
-//   if (nextEndTimeToUse > 0)
-//     params.endTime = nextEndTimeToUse;
+/**
+ *
+ * @param {callback} callback - Callback exetuted when once request finished
+ */
+function getMoreHistory(callback: Function) {
+  const params: HistoryAPIParams = { text: '', maxResults: 500 };
+  params.startTime = 0;
+  if (nextEndTimeToUse && nextEndTimeToUse > 0) params.endTime = nextEndTimeToUse;
 
-//   chrome.history.search(params, function(items) {
-//     let fetched = 0;
-//     for (let i = 0; i < items.length; i++) {
-//       let item = items[i];
-//       if (item.id in itemIdToIndex)
-//         continue;
-//       fetched += 1;
-//       allItems.push(item);
-//       itemIdToIndex[item.id] = allItems.length - 1;
-//     }
-//     if (items && items.length > 0) {
-//       nextEndTimeToUse = items[items.length-1].lastVisitTime;
-//     }
-//       callback(fetched);
-//   });
-//   return allItems;
-// }
+  chrome.history.search(params, function (items) {
+    let fetched = 0;
+    for (let i = 0; i < items.length; i++) {
+      const item = items[i];
+      if (item.id in itemIdToIndex) continue;
+      fetched += 1;
+      allItems.push(item);
+      itemIdToIndex[item.id] = allItems.length - 1;
+    }
+    if (items && items.length > 0) {
+      nextEndTimeToUse = items[items.length - 1].lastVisitTime;
+    }
+    callback(fetched);
+  });
+  return allItems;
+}
 
-// export function getHistories() {
-//     return getMoreHistory((cnt) => {
-//       console.log("got " + cnt);
-//       if (cnt > 0) {
-//         getHistories();
-//       }
-//     });
-// }
+export function getHistories() {
+  return getMoreHistory((cnt: number) => {
+    console.log('got ' + cnt);
+    if (cnt > 0) {
+      getHistories();
+    }
+  });
+}
 
-// export async function getHistoriesAsync() {
-//   return new Promise( (resolve, reject) => {
-//     getMoreHistory( cnt => {
-//       console.log('got '+ cnt);
-//       if (cnt > 0) {
-//         resolve(getHistoriesAsync());
-//       } else {
-//         resolve(allItems);
-//       }
-//     });
-//   })
-// }
+export async function getHistoriesAsync() {
+  return new Promise((resolve, reject) => {
+    getMoreHistory((cnt: number) => {
+      console.log('got ' + cnt);
+      if (cnt > 0) {
+        resolve(getHistoriesAsync());
+      } else {
+        resolve(allItems);
+      }
+    });
+  });
+}
