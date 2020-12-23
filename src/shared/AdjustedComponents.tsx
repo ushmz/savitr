f73db982ba1f4a862c505fdacfbea57f0c82ac91
Yusuke Shimizu <shimizu@design.inf.shizuoka.ac.jp>
2020-12-23 22:36:36 +0900

    :lipstick: Add components file

---
+++ src/shared/AdjustedComponents.tsx
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+export const RootContainer = styled.div`
+  width: 720px;
+  margin: auto;
+  padding-top: 20px;
+  padding-bottom: 20px;
+`;
+
+export const ExternalLinkText = styled.p`
+  color: #3132a9;
+  font-weight: bold;
+  font-size: 20px;
+`;
+
+export const HyperLinkText = styled.text`
+  color: #339d39;
+`;
+
+export const WarningText = styled.p`
+  background: #ff9999;
+  font-size: 20px;
+`;
+
+export const LinkedPageText = styled.p`
+  font-size: 18px;
+`;
