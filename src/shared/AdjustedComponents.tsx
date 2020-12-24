b10caa6af5cad6e411edda18998553116a4567cc
Yusuke Shimizu <shimizu@design.inf.shizuoka.ac.jp>
2020-12-25 01:32:55 +0900

    :+1: Add components

--- src/shared/AdjustedComponents.tsx
+++ src/shared/AdjustedComponents.tsx
@@ -1,5 +1,23 @@
 import styled from 'styled-components';
 
+export const SearchContainer = styled.div`
+  width: 720px;
+  margin-left: 30px;
+  padding-top: 60px;
+  padding-bottom: 20px;
+`;
+
+export const TitleText = styled.p`
+  color: #3132a9;
+  font-weight: bold;
+  font-size: 20px;
+`;
+
+export const URLText = styled.p`
+  color: #339d39;
+  margin-bottom: 3px;
+`;
+
 export const RootContainer = styled.div`
   width: 720px;
   margin: auto;
@@ -24,4 +42,17 @@ export const WarningText = styled.p`
 
 export const LinkedPageText = styled.p`
   font-size: 18px;
+`;
+
+export const SearchBarContainer = styled.div`
+  width: 480px;
+`;
+
+export const Inlinediv = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const Moveddiv = styled.div`
+  margin-left: -75px;
 `;
