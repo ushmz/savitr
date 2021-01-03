9df57f614ff9288aa1c7dff14ac102dda0e5bb69
Yusuke Shimizu <shimizu@design.inf.shizuoka.ac.jp>
2021-01-03 22:24:34 +0900

    🩹 Fix file imports

--- src/option/PostTask.tsx
+++ src/option/PostTask.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { MDBContainer, MDBTypography, MDBBtn } from 'mdbreact';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { dropAllDatabase } from '../repository/indexedDB';
+import { dropAllDatabase } from '../repository/xrayedIDB';
 import { ComponentLoader } from './internal/ComponentLoader';
 import { SetPageProp } from '../shared/types';
 
