d3cff054e1c40fbcb78ff965f3be4c53ba379f92
Yusuke Shimizu <shimizu@design.inf.shizuoka.ac.jp>
2021-01-03 16:29:54 +0900

    :lipstick: Integrating similar components

--- src/option/internal/ComponentLoader.tsx
+++ src/option/internal/ComponentLoader.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
 export const ComponentLoader = () => (
-  <div className="spinner-border" role="status">
-    <span className="sr-only" />
+  <div className="d-flex justify-content-center m-5" style={{ margin: 'auto' }}>
+    <div className="spinner-border text-primary" role="status">
+      <span className="sr-only">Loading...</span>
+    </div>
   </div>
 );
