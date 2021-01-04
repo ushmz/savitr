5b5205491193014f4e7eba9720d2929486c09100
Yusuke Shimizu <shimizu@design.inf.shizuoka.ac.jp>
2021-01-04 22:43:20 +0900

    :speech_balloon: Add discription comments

--- src/option/internal/AdjustedComponents.tsx
+++ src/option/internal/AdjustedComponents.tsx
@@ -1,5 +1,13 @@
 import styled from 'styled-components';
 
+/*****************************************
+ * Container components
+ *****************************************/
+
+/**
+ * Root component of search results.
+ * Adjust position and size here.
+ */
 export const SearchContainer = styled.div`
   width: 720px;
   margin-left: 30px;
@@ -7,26 +15,10 @@ export const SearchContainer = styled.div`
   padding-bottom: 20px;
 `;
 
-export const TitleText = styled.p`
-  color: #3132a9;
-  font-weight: bold;
-  font-size: 20px;
-`;
-
-export const URLText = styled.p`
-  color: #339d39;
-  margin-bottom: 3px;
-`;
-
-export const WarningText = styled.p`
-  background: #ff9999;
-  font-size: 20px;
-`;
-
-export const LinkedPageText = styled.p`
-  font-size: 18px;
-`;
-
+/**
+ * Root component for centralizing child elements.
+ * Adjust position and size here.
+ */
 export const RootContainer = styled.div`
   width: 720px;
   margin: auto;
@@ -34,25 +26,75 @@ export const RootContainer = styled.div`
   padding-bottom: 20px;
 `;
 
-export const ExternalLinkText = styled.p`
-  color: #3132a9;
-  font-weight: bold;
-  font-size: 20px;
-`;
-
-export const HyperLinkText = styled.text`
-  color: #339d39;
-`;
-
+/**
+ * Parent compnent for search bar in header.
+ * Adjust position and size here.
+ */
 export const SearchBarContainer = styled.div`
   width: 480px;
 `;
 
+/**
+ * Parent compnent for search bar icons in header.
+ * Used only to make child elements arrange side by side and centralize.
+ */
 export const Inlinediv = styled.div`
   display: flex;
   align-items: center;
 `;
 
+/**
+ * Parent compnent for search bar icons in header.
+ * Used only for adjusting position and size.
+ */
 export const Moveddiv = styled.div`
   margin-left: -75px;
+`;
+
+/*****************************************
+ * Text components
+ *****************************************/
+
+/**
+ * Colored text like Google search results' URL text.
+ */
+export const HyperLinkText = styled.p`
+  color: #339d39;
+`;
+
+/**
+ * Colored text like Google search results' title text.
+ * Adjust position and size here.
+ */
+export const TitleText = styled.p`
+  color: #3132a9;
+  font-weight: bold;
+  font-size: 20px;
+`;
+
+/**
+ * Colored text like Google search results' URL text.
+ * This is asjusted for SERP(`SearchResult.tsx`) usage.
+ * Expecting margin property, this is same to `HyperLinkText`.
+ */
+export const URLText = styled.p`
+  color: #339d39;
+  margin-bottom: 3px;
+`;
+
+/**
+ * Colored text for warning messages.
+ * Adjust position and size here.
+ */
+export const WarningText = styled.p`
+  background: #ff9999;
+  font-size: 20px;
+`;
+
+/**
+ * Texts for my proposal in custom SERP page.
+ * Adjust position and size here.
+ */
+export const LinkedPageText = styled.p`
+  font-size: 18px;
 `;
