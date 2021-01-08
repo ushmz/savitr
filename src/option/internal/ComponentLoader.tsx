08f522d32494f6b9a12c632d5ec61bd8f75fe1aa
Yusuke Shimizu <shimizu@design.inf.shizuoka.ac.jp>
2021-01-08 16:08:56 +0900

    :lipstick:  Add styling component

--- src/option/internal/ComponentLoader.tsx
+++ src/option/internal/ComponentLoader.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
-export const ComponentLoader = () => (
+export const ComponentLoaderCenter = () => (
   <div className="d-flex justify-content-center m-5" style={{ margin: 'auto' }}>
     <div className="spinner-border text-primary" role="status">
       <span className="sr-only">Loading...</span>
     </div>
+  </div>
+);
+
+export const ComponentLoader = () => (
+  <div className="spinner-border" role="status">
+    <span className="sr-only">Loading...</span>
   </div>
 );
