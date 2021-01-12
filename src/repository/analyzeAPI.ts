ca8aefb69566c151bbffb20ae908f03b38b5c9a8
Yusuke Shimizu <shimizu@design.inf.shizuoka.ac.jp>
2021-01-12 23:48:52 +0900

    :+1: Add url filter

--- src/repository/analyzeAPI.ts
+++ src/repository/analyzeAPI.ts
@@ -3,10 +3,27 @@ import { encryptText } from '../shared/util';
 
 const ENDPOINT = 'http://ec2-52-5-208-93.compute-1.amazonaws.com';
 
+const isNotNeed = (url: string): boolean => {
+  if (url.match(/.+(pdf|ppt|pptx|doc|docx|txt|rtf|xls|xlsx|jpg|jepg|png|gif)$/)) {
+    return true;
+  }
+  if (url.match(/https?:\/\/[a-zA-Z0-9]+\.google\.com\/.+/)) {
+    return true;
+  }
+  if (url.match(/^(?!http)/)) {
+    return true;
+  }
+  if (url.match(/https?:\/\/github\.com\/.+/)) {
+    return true;
+  }
+  return false;
+};
+
 export const analyse3pCookies = async (url: string): Promise<string[]> => {
+  if (isNotNeed(url)) return [];
   const response = await axios.post(
     `${ENDPOINT}/api/wbxr/analyze`,
-    { uid: localStorage.getItem('uid'), urls: encryptText(url) },
+    { uid: localStorage.getItem('uid'), url: encryptText(url) },
     { headers: { accept: 'application/json', 'Content-Type': 'application/json' } },
   );
   return response.data.cookies;
