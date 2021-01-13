9c8faaa6f16188931cc53ff288cc506f626d9536
Yusuke Shimizu <shimizu@design.inf.shizuoka.ac.jp>
2021-01-13 13:33:12 +0900

    :+1: Change API endpoint getting method
    - and Add URL filter

--- src/repository/analyzeAPI.ts
+++ src/repository/analyzeAPI.ts
@@ -1,9 +1,25 @@
 import axios from 'axios';
 
-const ENDPOINT = 'http://localhost:8000/api/wbxr';
-// const ENDPOINT = 'http://ec2-52-5-208-93.compute-1.amazonaws.com/api/wbxr';
+const ENDPOINT = process.env.API_ENDPOINT || 'http://localhost:8000';
 
-export const analyse3pCookies = async (url: string | string[]) => {
+const isNotNeed = (url: string): boolean => {
+  if (url.match(/.+(pdf|ppt|pptx|doc|docx|txt|rtf|xls|xlsx|jpg|jepg|png|gif)$/)) {
+    return true;
+  }
+  if (url.match(/https?:\/\/[a-zA-Z0-9]+\.google\.com\/.+/)) {
+    return true;
+  }
+  if (url.match(/^(?!http)/)) {
+    return true;
+  }
+  if (url.match(/https?:\/\/github\.com\/.*/)) {
+    return true;
+  }
+  return false;
+};
+
+export const analyse3pCookies = async (url: string): Promise<string[]> => {
+  if (isNotNeed(url)) return [];
   const response = await axios.post(
     `${ENDPOINT}/analyze`,
     { uid: localStorage.getItem('uid'), urls: url },
