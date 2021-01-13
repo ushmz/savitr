2cdaa3accb1c10c238cdc15b8de5b6207e8a1565
Yusuke Shimizu <shimizu@design.inf.shizuoka.ac.jp>
2021-01-13 22:34:42 +0900

    :speech_balloon: Update instruction literals and apearance

--- src/option/Introduction.tsx
+++ src/option/Introduction.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { MDBContainer, MDBTypography, MDBBtn } from 'mdbreact';
+import { MDBContainer, MDBTypography, MDBBtn, MDBRow, MDBCol } from 'mdbreact';
 import 'react-toastify/dist/ReactToastify.css';
 
 import { SetPageProp } from '../shared/types';
+import { SizedText } from './internal/AdjustedComponents';
 
 export const Introduction: React.FC<SetPageProp> = ({ setPage }) => {
   return (
@@ -12,25 +13,51 @@ export const Introduction: React.FC<SetPageProp> = ({ setPage }) => {
       <MDBTypography tag="h2" className="mt-5">
         タスク内容
       </MDBTypography>
-      <MDBTypography tag="p">
-        リモートでのやり取りが増えたので、ウェブカメラを購入したいと思ったとします。
-        どれを購入するか迷ったあなたは、ひとまずウェブカメラについての情報収集のために、ウェブ検索することにしました。
-        タスク画面では、「ウェブカメラ おすすめ」というワードでウェブ検索した際の検索結果画面を想定し、
-        自分が購入したいウェブカメラを検索してください。自分が購入したいウェブカメラが決まったら検索を終了してください。
-      </MDBTypography>
+      <SizedText size="18px" className="font-weight-bold lead">
+        リモートでのやり取りが増えたので、ウェブカメラの購入を考えています。
+        次に表示される検索結果から、自分が購入したいウェブカメラを選択してください。
+      </SizedText>
       <MDBTypography tag="h2" className="mt-5">
         注意事項
       </MDBTypography>
       <MDBTypography tag="ul">
         <li>検索クエリは変更できません。</li>
         <li>
-          検索結果は新規タブで開かれます。自由に閲覧していただいて構いませんが、新たなリンクをたどることはしないでください。
+          表示された検索結果リスト及び、そのリンク先のページのみ閲覧してください。検索結果は新規タブで開かれます。
         </li>
         <li>
           検索タスクの終了時には「購入したいと思った商品名」「一番参考になったページのURL」「決め手となった理由」
           の3点をお尋ねしますので、ご準備をお願いします。
         </li>
         <li>制限時間はありませんので、納得のいくまで検索を行ってください。</li>
+      </MDBTypography>
+      <MDBTypography tag="h2" className="mt-5">
+        検索結果下部の表示について
+      </MDBTypography>
+      <MDBTypography tag="p">
+        次に表示される画面では、Google検索やYahoo検索エンジンのような検索結果ページを模したページが表示されます。
+        各検索結果には、そのページを閲覧することで第三者に知られてしまう可能性のある閲覧履歴が表示されることがあります。
+      </MDBTypography>
+      <MDBRow className="my-5">
+        <MDBCol>
+          <p className="font-weight-bold">第三者に知られてしまう可能性のある情報がない場合</p>
+          <img src="./img/sample_result_unlinked.png" className="img-fluid z-depth-1" alt="" />
+        </MDBCol>
+        <MDBCol>
+          <p className="font-weight-bold">第三者に知られてしまう可能性のある情報がある場合</p>
+          <img src="./img/sample_result_linked.png" className="img-fluid z-depth-1" alt="" />
+        </MDBCol>
+      </MDBRow>
+      <MDBTypography tag="h3" className="mt-5">
+        検出方法
+      </MDBTypography>
+      <MDBTypography tag="p">
+        ウェブページには「cookie」と呼ばれる、情報を一時的に保存しておくための仕組みが用意されています。
+        本来はcookieに情報を保存したウェブページしか使うことはなく、ログイン情報の記録などに使用されることもあります。
+        しかし、情報を保存したウェブページ以外のページが内容を読み込める「サードパーティcookie」も存在しています。
+        主な使用例は広告です。サードパーティcookieに閲覧したページの情報などを保存しておき、広告を表示させる際にそのcookieを読み込むことで、
+        過去に閲覧したページを元に、より関連の高い広告を表示する仕組みがあります。実験システムでは過去に訪れたウェブページから
+        サードパーティcookieを検出することで、紐付けられる情報を判定しています。
       </MDBTypography>
       <div className="d-flex justify-content-center m-5" style={{ margin: 'auto' }}>
         <MDBBtn color="primary" className="float-right" onClick={() => setPage('Task')}>
