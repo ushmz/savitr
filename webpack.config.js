e181d7c46c3df337e261d22e50b532c18b777a0c
Yusuke Shimizu <shimizu@design.inf.shizuoka.ac.jp>
2021-01-13 22:27:00 +0900

    :wrench: Add build environments

--- webpack.config.js
+++ webpack.config.js
@@ -30,87 +30,92 @@ const nodeExternals = require('webpack-node-externals');
 
 const TerserPlugin = require('terser-webpack-plugin');
 
-module.exports = [
-  {
-    entry: {
-      popup: './src/popup/index.tsx',
-      option: [
-        './src/option/Attention.tsx', 
-        './src/option/Introduction.tsx',
-        './src/option/PostTask.tsx',
-        './src/option/PreTask.tsx',
-        './src/option/Task/index.tsx',
-        './src/option/index.tsx'
-      ],
-      background: './src/background/RuntimeMessageListener.ts'
-    },
-    output: {
-      path: __dirname + '/dist/savitr',
-    },
-    module: {
-      rules: [
-        {
-          test: /\.tsx?$/,
-          loader: 'awesome-typescript-loader',
-       },
-        {
-          test: /\.css/,
-          use: [
-            "style-loader",
+module.exports = (env) => {
+  const envFile = env ? `.env.${env.status}` : '.env.dev';
+  const distRoot = path.resolve(__dirname, 'dist', `savitr-${env.status}`);
+
+  return [
+    {
+      entry: {
+        popup: './src/popup/index.tsx',
+        option: [
+          './src/option/Attention.tsx', 
+          './src/option/Introduction.tsx',
+          './src/option/PostTask.tsx',
+          './src/option/PreTask.tsx',
+          './src/option/Task/index.tsx',
+          './src/option/index.tsx'
+        ],
+        background: './src/background/RuntimeMessageListener.ts'
+      },
+      output: {
+        path: distRoot,
+      },
+      module: {
+        rules: [
+          {
+            test: /\.tsx?$/,
+            loader: 'awesome-typescript-loader',
+        },
+          {
+            test: /\.css/,
+            use: [
+              "style-loader",
+              {
+                loader: "css-loader",
+                options: { url: false }
+              }
+            ]
+          }
+        ],
+      },
+      resolve: {
+        extensions: ['.ts', '.tsx', '.min.js', '.js', '.jsx'],
+        modules: [
+          path.resolve(__dirname, 'src', 'option'), 
+          path.resolve(__dirname, 'src', 'popup'), 
+          path.resolve(__dirname, 'src', 'service'), 
+          path.resolve(__dirname, 'src', 'shared'), 
+          'node_modules'
+        ]
+      },
+      plugins: [
+        new CopyWebpackPlugin({
+          patterns: [
             {
-              loader: "css-loader",
-              options: { url: false }
+              from: './public/savitri-manifest.json',
+              to: path.join(distRoot, 'manifest.json')
+            },
+            {
+              context: './public/img/samples',
+              from: '*.png',
+              to: path.join(distRoot, 'img')
+            },
+            {
+              context: 'db/serp/',
+              from: '*.csv',
+              to: path.join(distRoot, 'init', 'serp')
             }
           ]
-        }
+        }),
+        new HtmlWebpackPlugin({
+          template: './public/index.html',
+          filename: './index.html',
+          chunks: ['popup'],
+        }),
+        new HtmlWebpackPlugin({
+          template: './public/option.html',
+          filename: './option.html',
+          chunks: ['option'],
+        }),
+        new Dotenv({ path: envFile }),
+        new CleanWebpackPlugin()
       ],
-    },
-    resolve: {
-      extensions: ['.ts', '.tsx', '.min.js', '.js', '.jsx'],
-      modules: [
-        path.resolve(__dirname, 'src', 'option'), 
-        path.resolve(__dirname, 'src', 'popup'), 
-        path.resolve(__dirname, 'src', 'service'), 
-        path.resolve(__dirname, 'src', 'shared'), 
-        'node_modules'
-      ]
-    },
-    plugins: [
-      new CopyWebpackPlugin({
-        patterns: [
-          {
-            from: './public/savitri-manifest.json',
-            to: path.join(__dirname, 'dist', 'savitr', 'manifest.json')
-          },
-          {
-            context: 'db/dumped',
-            from: '*.csv',
-            to: path.join(__dirname, 'dist','savitr', 'init', 'xrayed')
-          },
-          {
-            context: 'db/serp/',
-            from: '*.csv',
-            to: path.join(__dirname, 'dist','savitr', 'init', 'serp')
-          }
-        ]
-      }),
-      new HtmlWebpackPlugin({
-        template: './public/index.html',
-        filename: './index.html',
-        chunks: ['popup'],
-      }),
-      new HtmlWebpackPlugin({
-        template: './public/option.html',
-        filename: './option.html',
-        chunks: ['option'],
-      }),
-      new Dotenv(),
-      new CleanWebpackPlugin()
-    ],
-    devServer: {
-      inline: true,
-      contentBase: path.join(__dirname, 'public'),
-      watchContentBase: true
+      devServer: {
+        inline: true,
+        contentBase: path.join(__dirname, 'public'),
+        watchContentBase: true
+      }
     }
-  }
-]
+  ]
+}
