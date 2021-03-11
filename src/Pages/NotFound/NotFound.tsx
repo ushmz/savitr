31cd35221fe23346205ef5f6746340e60633856f
Yusuke Shimizu <44394399+ushmz@users.noreply.github.com>
2021-03-11 18:12:16 +0900

    :+1: Add NotFound page (Need enhancement)

---
+++ src/Pages/NotFound/NotFound.tsx
@@ -0,0 +1,10 @@
+import { MDBContainer } from 'mdbreact';
+import React from 'react';
+
+export const NotFound: React.FC = () => {
+  return (
+    <MDBContainer>
+      <p>申し訳ありません．最初からやり直してください．</p>
+    </MDBContainer>
+  );
+};
---
+++ src/Pages/NotFound/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { NotFound as Component } from './NotFound';
+
+export const NotFound: React.FC = () => {
+  return <Component />;
+};
