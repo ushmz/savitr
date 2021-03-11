3dd8419fa0fb63ac4a6a58bcb957e25f180b21f0
Yusuke Shimizu <44394399+ushmz@users.noreply.github.com>
2021-03-11 17:52:32 +0900

    Update uploading page usability (#86)
    * :heavy_plus_sign: Add dependencies
    
    * :children_crossing: Add drag&drop interface
    
    * :+1: Add upload cancel function and single file mode
    
    * :lipstick: Update styles
    
    * :lipstick: Update style (margin)

--- src/Pages/Upload/Upload.tsx
+++ src/Pages/Upload/Upload.tsx
@@ -1,15 +1,80 @@
-import { MDBBtn } from 'mdbreact';
-import React from 'react';
+import { MDBBtn, MDBIcon, MDBCol, MDBContainer, MDBRow } from 'mdbreact';
+import React, { useState, useCallback } from 'react';
 import { useHistory } from 'react-router-dom';
+import { useDropzone } from 'react-dropzone';
 
 export const Upload: React.FC = () => {
   const history = useHistory();
+  const [uploaded, setUploaded] = useState<File[]>([]);
+
+  const onDrop = useCallback(
+    (acceptedFiles: File[]) => {
+      return setUploaded([...uploaded, ...acceptedFiles]);
+    },
+    [uploaded],
+  );
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop: onDrop, multiple: false });
+
+  const removeFile = (file: File) => () => {
+    const newFiles = [...uploaded];
+    newFiles.splice(newFiles.indexOf(file), 1);
+    setUploaded(newFiles);
+  };
+
+  const removeAll = () => {
+    setUploaded([]);
+  };
+
+  // eslint-disable-next-line react/jsx-key
+  const files = uploaded.map((file, idx) => {
+    return (
+      <div key={idx} className="border border-dark rounded-lg mx-3 my-1 px-3">
+        <MDBRow>
+          {/* <MDBProgress value={0} className="my-2" /> */}
+          <MDBCol md="10">
+            <div className="d-flex flex-row">
+              <input className="my-1" defaultValue={file.name} />
+              <p className="my-1">{`(${file.size} bytes)`}</p>
+            </div>
+          </MDBCol>
+          <MDBCol md="2">
+            <span className="float-right">
+              <MDBIcon icon="times-circle" onClick={removeFile(file)} />
+            </span>
+          </MDBCol>
+        </MDBRow>
+      </div>
+    );
+  });
+
   return (
-    <>
-      User Home
-      <MDBBtn color="primary" onClick={() => history.goBack()}>
-        User Home
-      </MDBBtn>
-    </>
+    <MDBContainer>
+      <p>User Home</p>
+      <p>実験協力ありがとうございます。こちらから履歴情報のアップロードを行ってください。</p>
+      <p>いただいた履歴情報は研究目的以外の用途で使用することはありません。</p>
+      <MDBRow className="border border-dark rounded-lg p-3">
+        {files.length !== 0 ? <div style={{ width: '100%' }}>{files}</div> : <></>}
+        {/* <div className="input-group-prepend">
+              <span className="input-group-text">ファイルを選択</span>
+            </div> */}
+        <div {...getRootProps({ className: 'custom-file my-3' })}>
+          <input {...getInputProps()} />
+          {isDragActive ? (
+            <p className="custom-file-label">ここにファイルをドラッグ&ドロップ。</p>
+          ) : (
+            <p className="custom-file-label">ファイルを選択するか、ドラッグ&ドロップしてください。</p>
+          )}
+        </div>
+      </MDBRow>
+      <MDBRow className="my-3">
+        <MDBBtn color="secondary" onClick={() => history.push('/user/rabhareit')}>
+          戻る
+        </MDBBtn>
+        <MDBBtn color="primary" onClick={() => history.push('/upload/done')}>
+          アップロードする
+        </MDBBtn>
+      </MDBRow>
+    </MDBContainer>
   );
 };
