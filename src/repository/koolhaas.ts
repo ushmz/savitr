dfb1d1d0803aa470183150b5e6958f3e3e74212e
Yusuke Shimizu <44394399+ushmz@users.noreply.github.com>
2021-03-16 16:21:06 +0900

    :alien: Add koolhaas API repository

---
+++ src/repository/koolhaas.ts
@@ -0,0 +1,46 @@
+import axios from "axios"
+
+const API_ENDPOINT = 'http://localhost:8080/v1';
+
+export type TaskInfo = {
+	query: string,
+        title: string,
+        description: string,
+        authorId: string,
+        searchUrl: string,
+        "type": string
+}
+
+export const fetchTaskInfo = async (taskId: number): Promise<TaskInfo|undefined> => {
+	const response = await axios.get(`${API_ENDPOINT}/savitr/tasks/${taskId}`);
+
+	if (response.status === 200) {
+		return response.data[0] as TaskInfo;
+	} else {
+		console.log("Error fetch error.");
+		return;
+	}
+} 
+
+export type Serp = {
+	id: string,
+        title: string,
+        url: string,
+        snippet: string,
+        cookies: {
+            "String": string,
+            "Valid": boolean
+        }
+}
+
+export const fetchSerp = async (taskId: number): Promise<Serp[] |undefined> => {
+	const response = await axios.get(`${API_ENDPOINT}/savitr/serps/${taskId}`);
+
+	if (response.status === 200) {
+		return response.data as Serp[];
+	} else {
+		console.log("Error fetch error.");
+		return;
+	}
+} 
+
