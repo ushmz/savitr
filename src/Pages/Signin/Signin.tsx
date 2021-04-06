d17f92782bdec6b68ff386ac7244fc601f691543
Yusuke Shimizu <44394399+ushmz@users.noreply.github.com>
2021-04-07 01:28:24 +0900

    :+1: Upgrade Signin page

--- src/Pages/Signin/Signin.tsx
+++ src/Pages/Signin/Signin.tsx
@@ -1,31 +1,86 @@
-import { MDBContainer, MDBCard, MDBCardTitle, MDBCardBody, MDBInputGroup, MDBBtn } from 'mdbreact';
-import React from 'react';
-import { Link } from 'react-router-dom';
+import { ErrorMessage } from '@hookform/error-message';
+import { useForm } from 'react-hook-form';
+import { MDBContainer, MDBCard, MDBCardTitle, MDBCardBody, MDBBtn } from 'mdbreact';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { useAuth } from 'shared/provider/authProvider';
+import { toast } from 'react-toastify';
+
+type SigninParam = {
+  externalId: string;
+  passwd: string;
+};
 
 export const Signin: React.FC = () => {
+  const { register, handleSubmit, formState, errors } = useForm<SigninParam>();
+  const [isLoading, setLoading] = useState<boolean>(false);
+  const history = useHistory();
+  const auth = useAuth();
+
+  const onSubmit = handleSubmit(({ externalId, passwd }) => {
+    setLoading(true);
+    const email = externalId + '@gmail.com';
+    auth
+      .signIn(email, passwd)
+      .then(() => {
+        setLoading(false);
+        history.push(`/user/${externalId}`);
+      })
+      .catch((e) => {
+        toast.error('ID または パスワードが正しくありません', e);
+        setLoading(false);
+      });
+  });
+
   return (
     <MDBContainer>
       <MDBCard className="mx-auto mt-5" style={{ width: '500px' }}>
         <MDBCardTitle className="mx-auto mt-3">ログイン</MDBCardTitle>
         <MDBCardBody>
-          <form action="" method="post">
-            <MDBInputGroup material containerClassName="mb-3 mt-0" label="ランサーズID" hint="ランサーズID" />
-            <MDBInputGroup
-              material
-              containerClassName="mb-3 mt-0"
+          <form onSubmit={onSubmit}>
+            <label htmlFor="externalId" className="grey-text font-weight-light">
+              ランサーズID（ユーザー名）
+            </label>
+            <input
+              id="externalId"
+              name="externalId"
+              ref={register({ required: true })}
+              className="mb-3 mt-0 form-control"
+            />
+            <ErrorMessage errors={errors} name="externalId" as="p" message="必須項目です" style={{ color: 'red' }} />
+
+            <label htmlFor="passwd" className="grey-text font-weight-light">
+              パスワード
+            </label>
+            <input
+              id="passwd"
+              name="passwd"
+              ref={register({ required: true, min: 8 })}
+              className="mb-3 mt-0 form-control"
               type="password"
-              label="パスワード"
-              hint="パスワード"
+            />
+            <ErrorMessage
+              errors={errors}
+              name="passwd"
+              as="p"
+              message="パスワードは必須項目かつ8文字以上が必要です"
+              style={{ color: 'red' }}
             />
             <div className="text-center">
               <MDBBtn type="submit" color="primary">
-                ログイン
+                {isLoading ? (
+                  <div className="spinner-border spinner-border-sm" role="status">
+                    <span className="sr-only">Loading...</span>
+                  </div>
+                ) : (
+                  'ログイン'
+                )}
               </MDBBtn>
             </div>
           </form>
         </MDBCardBody>
         <p className="mx-auto my-3" style={{ fontSize: '85%' }}>
-          アカウントをお持ちでない方は<Link to="/signup"> アカウント作成</Link>
+          アカウントをお持ちでない方は<Link to="/signup">アカウント登録</Link>
         </p>
       </MDBCard>
       <Link to="/user/rabhareit">
