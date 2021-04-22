c3f506fddae83ee1485c608b59599634effa633f
Yusuke Shimizu <44394399+ushmz@users.noreply.github.com>
2021-04-22 18:30:23 +0900

    :sparkles: Update to sign in with auth information in URL

--- src/Pages/Introduction/index.tsx
+++ src/Pages/Introduction/index.tsx
@@ -1,16 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { RouteComponentProps } from 'react-router-dom';
+import { parse } from 'query-string';
 import { Introduction as Component } from './Introduction';
 import tasks from '../../constants/tasks';
 import { ComponentLoaderCenter } from '../../Components/ComponentLoader';
+import { useAuth } from 'shared/provider/authProvider';
+import { useHistory } from 'react-router-dom';
+import { NotFound } from 'Pages/NotFound';
 
-type Props = RouteComponentProps<{ taskid?: string }>;
+type Props = RouteComponentProps<{
+  taskid?: string;
+}>;
 
-// TODO: Use react-router
 export const Introduction: React.FC<Props> = (props) => {
+  const auth = useAuth();
+  const history = useHistory();
+  const [credential, setCredential] = useState<boolean>();
+
   const taskId = props.match.params.taskid;
+  const searchParams = parse(props.location.search);
+  const extid = searchParams.user as string;
+  const secret = searchParams.secret as string;
+
+  useEffect(() => {
+    console.log(credential);
+    auth
+      .signIn(`${extid}@savitr.dummy.com`, secret)
+      .then(() => setCredential(true))
+      .catch(() => setCredential(false));
+  }, []);
+
   if (taskId === '1' || taskId === '2') {
-    return <Component task={tasks[+taskId - 1]} />;
+    if (credential === true) {
+      return <Component task={tasks[+taskId - 1]} />;
+    } else if (credential === undefined) {
+      return <ComponentLoaderCenter />;
+    } else {
+      return <NotFound />;
+    }
   } else {
     return <ComponentLoaderCenter />;
   }
