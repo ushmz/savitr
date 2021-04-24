2fa24b20afca649146e63c1dfb239416320d16bd
Yusuke Shimizu <44394399+ushmz@users.noreply.github.com>
2021-04-24 17:52:13 +0900

    :traffic_light: Fix routing

--- src/App/Routes.tsx
+++ src/App/Routes.tsx
@@ -10,11 +10,14 @@ import history from '../shared/browserHistory';
 import { Top } from '../Pages/Top';
 import { Signup } from '../Pages/Signup';
 import { Signin } from '../Pages/Signin';
+import { Register } from '../Pages/Register';
 import { User } from '../Pages/User';
 import { Upload } from '../Pages/Upload';
 // import { Download } from '../Pages/Download';
 import { NotFound } from '../Pages/NotFound';
 import { DownloadHistory } from '../Pages/HowTo/DownloadHistory';
+import { ExportHistory } from '../Pages/HowTo/ExportHistory';
+import { Completion } from '../Pages/Completion';
 
 const Routes = () => {
   return (
@@ -23,16 +26,19 @@ const Routes = () => {
         <Route exact path="/" component={Top}></Route>
         <Route exact path="/signin" component={Signin}></Route>
         <Route exact path="/signup" component={Signup}></Route>
+        <Route exact path="/register" component={Register}></Route>
 
         <Auth>
           <Route exact path="/introduction/:taskid" component={Introduction}></Route>
           <Route exact path="/attention" component={Attention}></Route>
-          <Route exact path="/how2exporthistory" component={DownloadHistory}></Route>
+          <Route exact path="/how2exporthistory/tko" component={DownloadHistory}></Route>
+          <Route exact path="/how2exporthistory/ext" component={ExportHistory}></Route>
           <Route exact path="/pretask" component={PreTask}></Route>
           <Route exact path="/posttask" component={PostTask}></Route>
           <Route exact path="/task/:taskid" component={Task}></Route>
-          <Route exact path="/user/" component={User}></Route>
-          <Route exact path="/user/upload" component={Upload}></Route>
+          <Route exact path="/user" component={User}></Route>
+          <Route exact path="/upload" component={Upload}></Route>
+          <Route exact path="/upload/completion" component={Completion}></Route>
           {/* <Route exact path="/user/:username/download/crx" component={Download}></Route> */}
         </Auth>
 
