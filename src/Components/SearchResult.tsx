77a211ded4728e5ceb730472eb90ac4bdf23aeb2
Yusuke Shimizu <44394399+ushmz@users.noreply.github.com>
2021-06-19 04:22:23 +0900

    :fire: Remove unnecessary code

--- src/Components/SearchResult.tsx
+++ src/Components/SearchResult.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
-// import { MDBBtn, MDBCollapse, MDBIcon, MDBPopper } from 'mdbreact';
-import { URLText, TitleText, SearchResultContainer, SizedText, WarningText } from './AdjustedComponents';
+import { URLText, TitleText, SearchResultContainer, SizedText } from './AdjustedComponents';
 import { truncateText } from '../shared/util';
-import { sendDocumentClickLog, sendHistoryClickLog } from '../repository/logAPI';
-// import { WARNING_MESSAGE } from '../shared/consts';
+import { sendDocumentClickLog } from '../repository/logAPI';
 import { LeakedPage } from '../shared/apis/apis';
 
 type Props = {
@@ -52,18 +50,14 @@ export const SearchResult: React.FC<Props> = ({
         <></>
       ) : (
         visible && (
-          <>
-            {/* <WarningText size="18px">{WARNING_MESSAGE}</WarningText> */}
-            <div className="m-3">
-              {/* <CollectedPages histories={leakedPages} documentURL={url} getTimeOnPage={getTimeOnPage} taskName={taskName} /> */}
-              <CollectedTendencyIconList
-                histories={leakedPages}
-                documentURL={url}
-                getTimeOnPage={getTimeOnPage}
-                taskName={taskName}
-              ></CollectedTendencyIconList>
-            </div>
-          </>
+          <div className="m-3">
+            <CollectedTendencyIconList
+              histories={leakedPages}
+              documentURL={url}
+              getTimeOnPage={getTimeOnPage}
+              taskName={taskName}
+            ></CollectedTendencyIconList>
+          </div>
         )
       )}
     </SearchResultContainer>
@@ -92,176 +86,3 @@ const CollectedTendencyIconList: React.FC<CollectedTendencyIconListProps> = (pro
 
   return <>{icons.slice(0, 5)}</>;
 };
-
-// type CollectedPagesProp = {
-//   histories: LeakedPage[];
-//   documentURL: string;
-//   getTimeOnPage: () => number;
-//   taskName: string;
-// };
-
-// const CollectedPages: React.FC<CollectedPagesProp> = ({ histories, documentURL, getTimeOnPage, taskName }) => {
-//   if (histories.length === 0) {
-//     return <></>;
-//   } else if (histories.length < 3) {
-//     return (
-//       <div className="m-3">
-//         <WarningText size="18px">{WARNING_MESSAGE}</WarningText>
-//         {histories.map((history, idx) => (
-//           <div
-//             key={idx}
-//             onClick={() =>
-//               sendHistoryClickLog({
-//                 uid: localStorage.getItem('uid') || '',
-//                 taskName: taskName,
-//                 timeOnPage: getTimeOnPage(),
-//                 linkedDocumentUrl: documentURL,
-//                 linkedPageNum: histories.length,
-//                 collapse: false,
-//               })
-//             }
-//           >
-//             <MDBPopper domElement placement="top">
-//               <a href={history.url} target="_blank" rel="noopener noreferrer">
-//                 <TextTruncate line={1} element="p" truncateText="..." text={history.title} />
-//               </a>
-//               <span>
-//                 <img src={history.thumb} width="240px" />
-//               </span>
-//             </MDBPopper>
-//           </div>
-//         ))}
-//       </div>
-//     );
-//   } else {
-//     const primary = histories[0];
-//     const secondly = histories[1];
-//
-//     return (
-//       <div className="m-3">
-//         <WarningText size="18px">{WARNING_MESSAGE}</WarningText>
-//         <div
-//           onClick={() =>
-//             sendHistoryClickLog({
-//               uid: chrome.runtime.id,
-//               taskName: taskName,
-//               timeOnPage: getTimeOnPage(),
-//               linkedDocumentUrl: documentURL,
-//               linkedPageNum: histories.length,
-//               collapse: false,
-//             })
-//           }
-//         >
-//           <MDBPopper domElement placement="top">
-//             <a href={primary.url} target="_blank" rel="noopener noreferrer">
-//               <TextTruncate line={1} element="p" truncateText="..." text={primary.title} />
-//             </a>
-//             <span>
-//               <img src={primary.thumb} width="240px" />
-//             </span>
-//           </MDBPopper>
-//         </div>
-//
-//         <div
-//           onClick={() =>
-//             sendHistoryClickLog({
-//               uid: chrome.runtime.id,
-//               taskName: taskName,
-//               timeOnPage: getTimeOnPage(),
-//               linkedDocumentUrl: documentURL,
-//               linkedPageNum: histories.length,
-//               collapse: false,
-//             })
-//           }
-//         >
-//           <MDBPopper domElement placement="top">
-//             <a href={secondly.url} target="_blank" rel="noopener noreferrer">
-//               <TextTruncate line={1} element="p" truncateText="..." text={secondly.title} />
-//             </a>
-//             <span>
-//               <img src={secondly.thumb} width="240px" />
-//             </span>
-//           </MDBPopper>
-//         </div>
-//         <CollapseMenu
-//           items={histories.slice(2)}
-//           documentURL={documentURL}
-//           getTimeOnPage={getTimeOnPage}
-//           taskName={taskName}
-//         />
-//       </div>
-//     );
-//   }
-// };
-
-type CollapseMenuProps = {
-  items: LeakedPage[];
-  documentURL: string;
-  getTimeOnPage: () => number;
-  taskName: string;
-};
-
-/**
- * Return Collapse menu component.
- * This is for the risk display area.
- */
-// const CollapseMenu: React.FC<CollapseMenuProps> = ({ items, documentURL, getTimeOnPage, taskName }) => {
-//   const [collapsedID, setCollapsedID] = React.useState<string>('');
-//
-//   const toggleCollapse = (collapseID: string) => () => {
-//     sendHistoryClickLog({
-//       uid: localStorage.getItem('uid') || '',
-//       taskName: taskName,
-//       timeOnPage: getTimeOnPage(),
-//       linkedDocumentUrl: documentURL,
-//       linkedPageNum: items.length + 2,
-//       collapse: true,
-//     });
-//     setCollapsedID((prevState) => {
-//       return prevState !== collapseID ? collapseID : '';
-//     });
-//   };
-//
-//   return (
-//     <>
-//       <MDBCollapse id={'hambgr'} isOpen={collapsedID}>
-//         {items.map((page, idx) => {
-//           return (
-//             <div
-//               key={idx}
-//               onClick={() =>
-//                 sendHistoryClickLog({
-//                   uid: localStorage.getItem('uid') || '',
-//                   taskName: taskName,
-//                   timeOnPage: getTimeOnPage(),
-//                   linkedDocumentUrl: documentURL,
-//                   linkedPageNum: items.length + 2,
-//                   collapse: false,
-//                 })
-//               }
-//             >
-//               <MDBPopper domElement placement="top">
-//                 <a href={page.url} target="_blank" rel="noopener noreferrer">
-//                   <TextTruncate line={1} element="p" truncateText="..." text={page.title} />
-//                 </a>
-//                 <span>
-//                   <img src={page.icon} width="240px" />
-//                 </span>
-//               </MDBPopper>
-//             </div>
-//           );
-//         })}
-//       </MDBCollapse>
-//       <MDBBtn className="mx-auto" color={'light-blue'} onClick={toggleCollapse('hambgr')}>
-//         <MDBIcon
-//           fixed
-//           icon="angle-down"
-//           className="mx-auto"
-//           onClick={toggleCollapse('hambgr')}
-//           flip={collapsedID === '' ? undefined : 'vertical'}
-//         />
-//         {collapsedID === '' ? `その他${items.length}件` : '非表示'}
-//       </MDBBtn>
-//     </>
-//   );
-// };
