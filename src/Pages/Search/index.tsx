039707928b64fe08fc3153b01b1abd5e4fb390be
Yusuke Shimizu <44394399+ushmz@users.noreply.github.com>
2021-06-21 23:49:02 +0900

    :lipstick: Update for google-search-ish style SERP

--- src/Pages/Search/index.tsx
+++ src/Pages/Search/index.tsx
@@ -1,16 +1,55 @@
 import React, { useState, useEffect } from 'react';
 import { RouteComponentProps } from 'react-router-dom';
-import { fetchSerp } from '../../shared/apis/apis';
-import { ExpTask as Component } from './Search';
-import { Serp } from '../../shared/apis/apis';
+import { useInterval } from 'use-interval';
+import { useStopwatch } from 'react-timer-hook';
+import { SearchResultPage as Component } from './Search';
+import { Serp, TaskInfo, fetchSerp, fetchTaskInfo, createTaskTimeLog } from '../../shared/apis/apis';
+import { useAuth } from 'shared/provider/authProvider';
 
 type SearchProp = RouteComponentProps<{ taskid: string }>;
 
 export const Search: React.FC<SearchProp> = (props) => {
+  const dummyTask: TaskInfo = {
+    id: 0,
+    conditionId: 0,
+    query: 'dummyQuery',
+    title: 'dummyTitle',
+    description: 'dummyDescription',
+    authorId: 'dummyAuthorId',
+    searchUrl: 'dummySearchUrl',
+    type: '',
+  };
+
+  const auth = useAuth();
+
   const taskIdNum = parseInt(props.match.params.taskid);
   const [serpPages, setSerpPages] = useState<Serp[]>([]);
+  const [offset, setOffset] = useState<number>(0);
+  const [task, setTask] = useState<TaskInfo>(dummyTask);
+  const { minutes, seconds } = useStopwatch({ autoStart: true });
+  const getTimeOnPage = () => minutes * 60 + seconds;
+
+  useInterval(async () => {
+    if (!window.document.hidden) {
+      await createTaskTimeLog({
+        id: auth.user?.email || 'void',
+        uid: auth.user?.uid || 'void',
+        timeOnPage: minutes * 60 + seconds,
+        url: document.URL,
+        taskId: taskIdNum,
+        conditionId: taskIdNum,
+      });
+    }
+  }, 1000);
+
   useEffect(() => {
+    fetchTaskInfo(taskIdNum).then((taskInfo) => {
+      taskInfo ? setTask(taskInfo) : setTask(dummyTask);
+    });
     fetchSerp(taskIdNum, 0).then((serp) => setSerpPages(serp));
-  }, []);
-  return <Component pageList={serpPages} />;
+    window.scrollTo(0, 0);
+  }, [offset]);
+  return (
+    <Component offset={offset} setOffset={setOffset} pageList={serpPages} task={task} getTimeOnPage={getTimeOnPage} />
+  );
 };
--- src/shared/consts.ts
+++ src/shared/consts.ts
@@ -1,3 +1,16 @@
 export const SERP_MIN_PAGE = 1;
 export const SERP_MAX_PAGE = 10;
 export const WARNING_MESSAGE = '以下のようなページが知られます。';
+
+export const CROSS_SVG_PATH = `
+M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 
+17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z
+`;
+
+export const SEARCH_SVG_PATH = `
+M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 
+9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 
+4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 
+0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 
+9.5 11.99 14 9.5 14z
+`;
