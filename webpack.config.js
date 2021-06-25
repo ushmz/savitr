f89239edb30124a7cde6fae16369e2a8e67a6aed
Yusuke Shimizu <44394399+ushmz@users.noreply.github.com>
2021-06-25 13:47:17 +0900

    :boom: Fix typescript loader

--- webpack.config.js
+++ webpack.config.js
@@ -7,27 +7,6 @@ const Dotenv = require('dotenv-webpack');
 
 const nodeExternals = require('webpack-node-externals');
 
-/*
- * SplitChunksPlugin is enabled by default and replaced
- * deprecated CommonsChunkPlugin. It automatically identifies modules which
- * should be splitted of chunk by heuristics using module duplication count and
- * module category (i. e. node_modules). And splits the chunks…
- *
- * It is safe to remove "splitChunks" from the generated configuration
- * and was added as an educational example.
- *
- * https://webpack.js.org/plugins/split-chunks-plugin/
- *
- */
-
-/*
- * We've enabled TerserPlugin for you! This minifies your app
- * in order to load faster and run less javascript.
- *
- * https://github.com/webpack-contrib/terser-webpack-plugin
- *
- */
-
 const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = () => {
@@ -42,7 +21,7 @@ module.exports = () => {
       rules: [
         {
           test: /\.tsx?$/,
-          loader: 'awesome-typescript-loader',
+          loader: 'ts-loader',
         },
         {
           test: /\.css$/,
