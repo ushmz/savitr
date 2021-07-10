271467e3e361389c2acfd98f7f4834cf3549cd81
Yusuke Shimizu <44394399+ushmz@users.noreply.github.com>
2021-07-10 23:10:06 +0900

    :+1: Fix up routes

--- src/App/Routes.tsx
+++ src/App/Routes.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Route, Router, Switch } from 'react-router-dom';
 import { Auth } from '../Pages/Auth/Auth';
+import { Completion } from '../Pages/Completion';
 import { Introduction } from '../Pages/Introduction';
 import { PostTask } from '../Pages/PostTask';
 import { PreTask } from '../Pages/PreTask';
 import { Task } from '../Pages/Task';
 import { Search } from '../Pages/Search';
 import { Top } from '../Pages/Top';
-import { User } from '../Pages/User';
 import { NotFound } from '../Pages/NotFound';
 import history from '../shared/browserHistory';
 
@@ -23,7 +23,7 @@ const Routes: React.FC = () => {
           <Route exact path="/posttask" component={PostTask}></Route>
           <Route exact path="/task/:taskid" component={Task}></Route>
           <Route exact path="/search/:taskid" component={Search}></Route>
-          <Route exact path="/user" component={User}></Route>
+          <Route exact path="/compcode" component={Completion}></Route>
         </Auth>
 
         <Route path="*" component={NotFound} />
