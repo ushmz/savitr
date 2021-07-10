b0df89807e898ab235eabfe306f3efc1541409fe
Yusuke Shimizu <44394399+ushmz@users.noreply.github.com>
2021-07-10 23:02:44 +0900

    :+1: Fix up pre/post enquete screen

--- src/Pages/PostTask/PostTask.tsx
+++ src/Pages/PostTask/PostTask.tsx
@@ -1,35 +1,35 @@
-import React from 'react';
-import { MDBContainer, MDBTypography, MDBBtn } from 'mdbreact';
+import React, { useState } from 'react';
+import { MDBContainer, MDBBtn } from 'mdbreact';
+import { getPostTaskEnqueteByGroupId } from '../../shared/util';
 
 export const PostTask: React.FC = () => {
-  const uid = localStorage.getItem('uid');
+  const [clicked, isClicked] = useState<boolean>(false);
+  const group = localStorage.getItem('group') || '';
+  const enquete = getPostTaskEnqueteByGroupId(group);
 
   return (
     <MDBContainer className="my-5">
-      <MDBTypography tag="h1">事後アンケート</MDBTypography>
-      <MDBTypography tag="p">
-        事後アンケートにお答えください。質問は全部で17問あり、想定所要時間は約5分です。
-      </MDBTypography>
-      <MDBTypography tag="h2" className="mt-5">
-        注意事項
-      </MDBTypography>
-      <MDBTypography tag="p">
+      <h1>事後アンケート</h1>
+      <p>事後アンケートにお答えください。質問は全部で17問あり、想定所要時間は約5分です。</p>
+      <h2 className="mt-5">注意事項</h2>
+      <p>
         アンケートページは別タブで開かれますが、アンケートページが開いてもこのページは
         <strong className="font-weight-bold">開いたままに</strong>してください。
+      </p>
+      <p>
         アンケートへの回答が終了したらアンケートページが表示されているタブを閉じ、この画面から実験を再開してください。
-      </MDBTypography>
-      <a
-        className="white-text"
-        // Or import from `src/shared/config.ts`
-        href={`https://docs.google.com/forms/d/e/1FAIpQLSe-S36BVjkqZi-rNth3_lITdpMlhLXs3HRjcimf1RetXU8y4A/viewform?usp=pp_url&entry.183922479=${uid}`}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <MDBBtn color="primary" className="mb-5">
+      </p>
+      <a className="white-text" href={enquete} target="_blank" rel="noopener noreferrer">
+        <MDBBtn color="primary" className="mb-5" onClick={() => isClicked(true)}>
           アンケートページへ
         </MDBBtn>
       </a>
-      <MDBTypography tag="p">実験は以上で終了となります。ご協力ありがとうございました。</MDBTypography>
+      <p>実験は以上で終了となります。以下のボタンをクリックして完了コードを発行してください。</p>
+      <a href="/compCode" className="white-text">
+        <MDBBtn color="primary" className={`${clicked ? '' : 'disabled'} mb-5`}>
+          完了コード発行
+        </MDBBtn>
+      </a>
     </MDBContainer>
   );
 };
